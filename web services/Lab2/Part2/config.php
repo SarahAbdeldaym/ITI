caadea8059c25238c99709b027113e2de8a41054
SarahAbdeldaym <97462340+SarahAbdeldaym@users.noreply.github.com>
2022-03-26 19:57:48 +0200

    web update

--- web services/Lab2/Part2/config.php
+++ web services/Lab2/Part2/config.php
@@ -2,6 +2,6 @@
 
 define("Driver", "mysql");
 define("Host", "localhost");
-define("Database", "glasses_store");
+define("Database", "northwind");
 define("Username", "root");
 define("Password", "");
