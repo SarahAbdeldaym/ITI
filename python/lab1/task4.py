232168c20f48e8ed2e7cf7b3c489b0315958e21b
Sarah Abdeldaym <97462340+SarahAbdeldaym@users.noreply.github.com>
2022-05-28 00:08:39 +0200

    Delete python/lab1 directory

--- python/lab1/task4.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-with open('task4.txt', 'r') as fileObj:
-    word_list = fileObj.read().replace('\n', '').split()
-
-top_20_Words = Counter(word_list).most_common(20)
-
-print(top_20_Words)
